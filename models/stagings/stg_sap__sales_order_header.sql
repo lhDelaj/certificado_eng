5355f02dde36346fda70b0b5c5570522788371b7
lucas dela justina <lhdelajustina@gmail.com>
2023-11-10 19:31:51 +0000

    fixed dates still missing yml

--- models/stagings/stg_sap__sales_order_header.sql
+++ models/stagings/stg_sap__sales_order_header.sql
@@ -9,9 +9,9 @@ with
             , cast (salespersonid as int) as sales_person_id
             , cast(shiptoaddressid as int) as ship_to_address_id
             , cast(status as int) as status
-            , cast(shipdate as string) as ship_date
-            , cast(orderdate as string) as order_date
-            , cast(duedate as string) as due_date
+            , DATE(shipdate) as ship_date
+            , DATE(orderdate) as order_date
+            , DATE(duedate) as due_date
             , cast(subtotal as numeric) as subtotal
             , cast(taxamt as numeric) as tax_amount
             , cast(freight as numeric) as freight
